221b4a65af07fff754ce5be660675e65b5f2425a
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-05-16 00:50:51 +1000

    Add files via upload

---
+++ Homework2.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "rus");
+	std::wcout << L"Введите слово:\n" << std::endl;
+	std::string word;
+	std::cin >> word;
+	std::wcout << L"Вы ввели:\n" << std::endl;
+	std::cout << word << "\n";
+}
